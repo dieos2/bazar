597b39fde0b77168ca2c99c5409b02a9335e8d8b
dieos2 <dieos2@gmail.com>
2017-02-22 10:26:06 -0300

    Scanner

--- views/venda/view.php
+++ views/venda/view.php
@@ -9,6 +9,16 @@ use app\models\Setup;
 
 
 ?>
+<style>
+    #outdiv
+{
+    background-color: #000;
+    width:100%;
+    height: 200px;
+	border: solid;
+	
+}
+</style>
   <section class="vbox">          
             <section class="scrollable padder">
               <ul class="breadcrumb no-border no-radius b-b b-light pull-in">
